e066f02468de21cf00e1d2a4ee92a0748b982e3f
Angele <anjelkayazade@gmail.com>
2022-10-09 19:46:51 -0400

    Wrote findStudentByName function

--- src/solution.js
+++ src/solution.js
@@ -2,6 +2,13 @@
   Write a function that returns a particular student when given a name. If no name matches, return `null`.
 */
 
-function findStudentByName(students, name) {}
+function findStudentByName(students, name) {
+  for (let i = 0; i<students.length; i++){
+    if(students[i].name == name) {
+      return students[i];
+    }
+  }
+  return null;
+}
 
 module.exports = findStudentByName;
